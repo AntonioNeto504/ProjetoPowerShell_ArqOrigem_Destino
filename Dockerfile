2408c1ff5eb66326bf29c25f55168fb3e192a668
AntonioNeto504 <antoniocustorioneto@gmail.com>
2024-04-16 20:27:59 -0300

    Adicionando script PowerShell

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+# Use a imagem oficial do PowerShell
+FROM mcr.microsoft.com/powershell:latest
+
+# Copie o script para o contêiner
+COPY ScriptPw.ps1 /scripts/
+
+# Execute o script ao iniciar o contêiner
+CMD ["pwsh", "-File", "/scripts/ScriptPw.ps1"]
--- ScriptPw.ps1
+++ ScriptPw.ps1
@@ -1,22 +1,23 @@
 # Define os caminhos das pastas de origem e destino
-$origem = "C:\Caminho\da\Pasta\de\Origem"
-$destinoTextos = "C:\Caminho\da\Pasta\de\Destino\Textos"
-$destinoImagens = "C:\Caminho\da\Pasta\de\Destino\Imagens"
-$destinoPDFs = "C:\Caminho\da\Pasta\de\Destino\PDFs"
-$destinoOutros = "C:\Caminho\da\Pasta\de\Destino\Outros"
+$origem = "C:\Origem"
+$destinoTextos = "C:\Destino"
+$destinoImagens = "C:\Destino\Imagens"
+$destinoPDFs = "C:\Destino\PDFs"
+$destinoOutros = "C:\Destino\Outros"
 
+# Verifica se as pastas de destino existem e, se não, as cria
+if (-not (Test-Path $destinoTextos)) { New-Item -ItemType Directory -Path $destinoTextos -Force }
+if (-not (Test-Path $destinoImagens)) { New-Item -ItemType Directory -Path $destinoImagens -Force }
+if (-not (Test-Path $destinoPDFs)) { New-Item -ItemType Directory -Path $destinoPDFs -Force }
+if (-not (Test-Path $destinoOutros)) { New-Item -ItemType Directory -Path $destinoOutros -Force }
 
-if (-not (Test-Path $destinoTextos)) { New-Item -ItemType Directory -Path $destinoTextos }
-if (-not (Test-Path $destinoImagens)) { New-Item -ItemType Directory -Path $destinoImagens }
-if (-not (Test-Path $destinoPDFs)) { New-Item -ItemType Directory -Path $destinoPDFs }
-if (-not (Test-Path $destinoOutros)) { New-Item -ItemType Directory -Path $destinoOutros }
-
+# Obtém a lista de arquivos na pasta de origem
 $arquivos = Get-ChildItem -Path $origem
 
 # Para cada arquivo na pasta de origem
 foreach ($arquivo in $arquivos) {
     # Determina a extensão do arquivo
-    $extensao = $arquivo.Extension.TrimStart('.')
+    $extensao = $arquivo.Extension.ToLower()
 
     switch -Wildcard ($extensao) {
         "*.txt" { Move-Item -Path $arquivo.FullName -Destination $destinoTextos }
@@ -25,3 +26,5 @@ foreach ($arquivo in $arquivos) {
         default { Move-Item -Path $arquivo.FullName -Destination $destinoOutros }
     }
 }
+
+Write-Host "Arquivos movidos com sucesso!"
